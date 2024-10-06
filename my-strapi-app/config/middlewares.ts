e0e1505ede200cdeebba75ed51cf1fa48af62524
paskal11 <lysenko.volodymyr@yota-x.com>
2024-10-06 17:40:02 +0300

    firs

---
+++ my-strapi-app/config/middlewares.ts
@@ -0,0 +1,17 @@
+export default [
+  'strapi::logger',
+  'strapi::errors',
+  'strapi::security',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['http://localhost:3000'], // Укажите адрес вашего React-приложения
+    },
+  },
+  'strapi::poweredBy',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+];
