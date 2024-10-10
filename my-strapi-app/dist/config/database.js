d9207382857fa33298b12a0ce1dcce2eb2c049ad
paskal11 <lysenko.volodymyr@yota-x.com>
2024-10-10 12:54:45 +0300

    add db postgres

--- my-strapi-app/dist/config/database.js
+++ my-strapi-app/dist/config/database.js
@@ -27,23 +27,17 @@ exports.default = ({ env }) => {
         },
         postgres: {
             connection: {
-                connectionString: env('DATABASE_URL'),
-                host: env('DATABASE_HOST', 'localhost'),
-                port: env.int('DATABASE_PORT', 5432),
-                database: env('DATABASE_NAME', 'strapi'),
-                user: env('DATABASE_USERNAME', 'strapi'),
-                password: env('DATABASE_PASSWORD', 'strapi'),
-                ssl: env.bool('DATABASE_SSL', false) && {
-                    key: env('DATABASE_SSL_KEY', undefined),
-                    cert: env('DATABASE_SSL_CERT', undefined),
-                    ca: env('DATABASE_SSL_CA', undefined),
-                    capath: env('DATABASE_SSL_CAPATH', undefined),
-                    cipher: env('DATABASE_SSL_CIPHER', undefined),
-                    rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', true),
+                connectionString: env('DATABASE_URL', process.env.DATABASE_URL),
+                host: env('DATABASE_HOST', process.env.DATABASE_HOST),
+                port: env.int('DATABASE_PORT', process.env.DATABASE_PORT),
+                database: env('DATABASE_NAME', process.env.DATABASE_NAME),
+                user: env('DATABASE_USERNAME', process.env.DATABASE_USERNAME),
+                password: env('DATABASE_PASSWORD', process.env.DATABASE_PASSWORD),
+                ssl: {
+                    rejectUnauthorized: env.bool('DATABASE_SSL', false),
                 },
-                schema: env('DATABASE_SCHEMA', 'public'),
             },
-            pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
+            acquireConnectionTimeout: env.int('DATABASE_CONNECTION_TIMEOUT', 60000),
         },
         sqlite: {
             connection: {
